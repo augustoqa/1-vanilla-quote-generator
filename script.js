8553aae3a115f8f38218a7e3b47c0de2e729dd51
Cesar Acual <chechaacual@gmail.com>
2022-02-27 12:42:18 -0600

    Show quote and add functionallity to the buttons

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+const quoteContainer = document.getElementById('quote-container')
+const quoteText = document.getElementById('quote')
+const authorText = document.getElementById('author')
+const twitterBtn = document.getElementById('twitter')
+const newQuoteBtn = document.getElementById('new-quote')
+
 // Get Quote From API
 async function getQuote() {
 	const apiUrl = 'https://api.quotable.io/random'
@@ -5,11 +11,33 @@ async function getQuote() {
 	try {
 		const response = await fetch(apiUrl)
 		const data = await response.json()
+
+		// Reduce font size for long quotes
+		if (data.content.length > 50) {
+			quoteText.classList.add('long-quote')
+		} else {
+			quoteText.classList.remove('long-quote')
+		}
+
+		authorText.innerText = data.author
+		quoteText.innerText = data.content
 		console.log(data)
 	} catch (error) {
 		console.log('whoops, no quote', error)
 	}
 }
+
+// Tweet Quote
+function tweetQuote() {
+	const quote = quoteText.innerText
+	const author = authorText.innerText
+	const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`
+	window.open(twitterUrl, '_blank')
+}
+
+// Event Listener
+newQuoteBtn.addEventListener('click', getQuote)
+twitterBtn.addEventListener('click', tweetQuote)
 
 // On Load
 getQuote()
